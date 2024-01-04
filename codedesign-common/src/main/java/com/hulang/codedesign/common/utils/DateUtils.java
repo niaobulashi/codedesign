b5811e6acc33003366d75d1275219982320061ac
hulang <hulang6666@qq.com>
2024-01-04 16:33:56 +0800

    new changes

---
+++ codedesign-common/src/main/java/com/hulang/codedesign/common/utils/DateUtils.java
@@ -0,0 +1,10 @@
+package com.hulang.codedesign.common.utils;
+
+/**
+ * @description: DateUtils
+ * @date: 2024/1/4 10:25
+ * @author: HuLang
+ * @version: 1.0
+ */
+public class DateUtils {
+}
---
+++ codedesign-concurrency/src/main/java/com/hulang/codedesign/concurrency/MutilThreadApplication.java
@@ -0,0 +1,13 @@
+package com.hulang.codedesign.concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutilThreadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MutilThreadApplication.class, args);
+    }
+
+}
